a49c0246a70d0b5254e0607eefaddf7550d8204b
Student <student@TesterVM-42.edu-wsb.local>
2020-11-29 10:31:21 +0100

    po dopisaniu programu i test case'ow

---
+++ program.py
@@ -0,0 +1,14 @@
+#program.py
+
+from random import randint
+
+def zwroc_100():
+    return 100
+
+
+def zwroc_50():
+    return 49
+
+def rand_100():
+    x = randint(0,101)
+    return x
--- test_program.py
+++ test_program.py
@@ -9,18 +9,19 @@ class TestProgram(unittest.TestCase):
     def test_zwroc_100(self):
 
 
-        wynik = program.test_zwroc_100()
+        wynik = program.zwroc_100()
         self.assertEqual(wynik, 100)
 
 
     def test_inna_metoda(self):
 
-        pass
-
+        wynik = program.zwroc_50()
+        self.assertEqual(wynik, 50)
 
     def test_inna_metoda_2(self):
-
-        pass
+        for x in range (0, 10):
+            wynik = program.rand_100()
+            self.assertLessEqual(wynik, 100)
 
 
 
